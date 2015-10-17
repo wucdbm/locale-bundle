b10ade8d361ed76544d5489fa699d47f4a00eb59
Martin Kirilov <wucdbm@gmail.com>
2015-10-18 00:30:41 +0300

    validation

--- Form/Type/LocaleType.php
+++ Form/Type/LocaleType.php
@@ -5,31 +5,44 @@ namespace Wucdbm\Bundle\LocaleBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class LocaleType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
             ->add('locale', 'text', [
-                'label' => 'Locale',
-                'attr'  => [
+                'label'       => 'Locale',
+                'attr'        => [
                     'placeholder' => 'Locale'
+                ],
+                'constraints' => [
+                    new NotBlank()
                 ]
             ])
             ->add('name', 'text', [
-                'label' => 'Name',
-                'attr'  => [
+                'label'       => 'Name',
+                'attr'        => [
                     'placeholder' => 'Name'
+                ],
+                'constraints' => [
+                    new NotBlank()
                 ]
             ])
             ->add('isEnabled', 'checkbox', [
-                'label'    => 'Is Enabled',
-                'required' => false
+                'label'       => 'Is Enabled',
+                'required'    => false,
+                'constraints' => [
+                    new NotBlank()
+                ]
             ])
             ->add('currency', 'text', [
-                'label' => 'Currency',
-                'attr'  => [
+                'label'       => 'Currency',
+                'attr'        => [
                     'placeholder' => 'Currency'
+                ],
+                'constraints' => [
+                    new NotBlank()
                 ]
             ]);
     }
